e738980815a7ad2c93e3c7a1f9ec179d494f672c
franPzTo <franperezt1@gmail.com>
2025-08-09 12:09:54 -0300

    punto 8 resuelto

---
+++ Practica/js/Practica_8.js
@@ -0,0 +1,8 @@
+let x= prompt("Escribe un numero");
+x= parseInt(x);
+if(x % 2 === 0){
+    document.write("El "+x+" es divisible por 2");
+}
+else{
+    document.write("El "+x+" NO es divisible por 2");
+}
