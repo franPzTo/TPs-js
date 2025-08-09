1c77c17ae88270001c4bbc0ccffec134e6a6cedf
franPzTo <franperezt1@gmail.com>
2025-08-09 12:09:17 -0300

    punto 7 resuelto

---
+++ Practica/js/Practica_7.js
@@ -0,0 +1,4 @@
+let a= prompt("Escribe 1°N"); let b= prompt("Escribe 2°N"); let c= prompt("Escribe 3°N");
+resultado= Math.max(a, b, c);
+resultado2= Math.min(a, b, c);
+document.write("El "+ resultado +" es el número más grande. Y "+ resultado2 + " es el numero menor. entre " + a +", "+ b +" y "+ c )
