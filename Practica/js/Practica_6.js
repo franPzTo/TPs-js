f64586f1fb74207436c029da8b8f3341cbe1041c
franPzTo <franperezt1@gmail.com>
2025-08-09 12:08:26 -0300

    punto 6 resuelto

---
+++ Practica/js/Practica_6.js
@@ -0,0 +1,4 @@
+let a= prompt("Escribe 1°N"); let b= prompt("Escribe 2°N");
+resultado= Math.max(a, b);
+resultado2= Math.min(a, b);
+document.write("El "+ resultado +" es el número más grande. Y "+ resultado2 + " es el numero menor" )
